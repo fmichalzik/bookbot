1bb64292b2337047ad6f14e26874486c10fee7e8
fmichalzik <51121813+fmichalzik@users.noreply.github.com>
2025-01-25 21:56:42 +0100

    Added feature so the user can select a file from the books directory

--- main.py
+++ main.py
@@ -1,26 +1,59 @@
+import os
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = read_file(book_path)
-    num_words = count_words(text)
-    
-    # Create dictionary with count per character in text
-    chars_dictionary = count_chars(text)
-
-    # Create a list for dictionaries
-    list_with_chars_dictionaries = convert(chars_dictionary)
-    # Sort the list for dictionaries
-    list_with_chars_dictionaries.sort(reverse=True, key=sort_on)
-
+    ################ WELCOME ################
+    print("Welcome to the Bookbot!")
+    print("This program will analyze the text of a book and report the number of words\nand the number of times each alphabetical character appears in the text.")
     
 
-    ################ REPORT ################
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print()
-    report_characters_found(list_with_chars_dictionaries)
-    print("--- End report ---")
+    # Creates a list with the .txt files in in books/ directory so it can be prompted
+    books_directory = "books"
+    books = []
+    for filename in os.listdir(books_directory):
+        if filename.endswith(".txt"):
+            books.append(filename)
+
+    # Input handeling, so the user can either input a filename from the shown list or 'exit'
+    # while == True -> reprompting the input to the user when the file does not exist or a report was generated
+    while True:
+        print()
+        print("Which book would you like to analyze?")
+        print("There are currently the following files in the 'books' directory:")
+        for book in books: 
+            print(f"- {book}")
+        selected_book = input("Please enter the name of the file (including the .txt extension) or 'exit': ")
+        print()
+        # so the user can exit the input and end the program
+        if selected_book == "exit":
+            print("Goodbye!")
+            return False
+
+        # if file does not exist --> input reprompted as while input_sate still True:
+        if selected_book not in books:
+            print("The file you entered does not exist in the 'books' directory.")
+
+        # if the file exists the program will proceed
+        else:       
+            book_path = "books/" + selected_book
+            text = read_file(book_path)
+            num_words = count_words(text)
+
+            # Create dictionary with count per character in text
+            chars_dictionary = count_chars(text)
+
+            # Create a list for dictionaries
+            list_with_chars_dictionaries = convert(chars_dictionary)
+            # Sort the list for dictionaries
+            list_with_chars_dictionaries.sort(reverse=True, key=sort_on)
 
 
+
+            ################ REPORT ################
+            print(f"--- Begin report of {book_path} ---")
+            print(f"{num_words} words found in the document")
+            print()
+            report_characters_found(list_with_chars_dictionaries)
+            print("--- End report ---")
 
 # Returns the text of a file (defined by filepath) as a string 
 def read_file(path):
